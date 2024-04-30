f0398fb052ead57ea1116d33ce3df82e3dc566bb
Morgan Casale <morgan.casale@gmail.com>
2024-04-30 16:48:46 +0200

    Cleaning

--- prepare_for_overleaf.py
+++ prepare_for_overleaf.py
@@ -1,12 +1,12 @@
 import os
 import shutil
-from rounding import round_up
+#from math import ceil
 
 source_folder = os.getcwd()
 out_folder = "../overleaf"
 
 dir_to_delete = ["__pycache__", ".git", "svg-inkscape"]
-file_ext_to_keep = [".tex", ".bib", ".cls", ".svg"] # ".png"
+file_ext_to_keep = [".tex", ".bib", ".cls", ".svg", ".png", ".jpg"] # ".png"
 str_of_file_to_delete = ["Example", "Template"]
 
 def on_rmtree_error(func, path, exc_info):
